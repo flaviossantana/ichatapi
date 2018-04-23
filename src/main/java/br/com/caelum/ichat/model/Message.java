b1e98fcf05994421322f971ad0dab26cf6fe8d21
flavio.santana <9090flavio@gmail.com>
2018-04-23 20:21:58 -0300

    commit

---
+++ src/main/java/br/com/caelum/ichat/model/Message.java
@@ -0,0 +1,16 @@
+package br.com.caelum.ichat.model;
+
+public class Message {
+
+	private String text;
+	private Integer id;
+
+	public String getText() {
+		return text;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+}
